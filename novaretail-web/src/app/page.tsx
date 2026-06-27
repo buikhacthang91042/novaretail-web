f2e8585be262093cb827b6e8ba54a4c836c2cae2
buikhacthang91042 <buikhacthang910.42@gmail.com>
2026-06-27 22:46:41 +0700

    feat(init): initialize nextjs application

---
+++ novaretail-web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center ">
+      <h1 className="text-red-500">Coming Soon</h1>
+    </div>
+  );
+}
